875226c4d2d6d4b65d594f084d3ed130a2c80185
student <student@GBTC440J15LX.montgomerycollege.edu>
2017-07-25 09:36:19 -0400

    This is Fun no. game

---
+++ src/numberfun/NoFun.java
@@ -0,0 +1,16 @@
+package numberfun;
+
+public class NoFun {
+
+	public static void main(String[] args) {
+		for (int i=1;i<=100;i++){
+		if(i % 2 ==0){
+			System.out.println( i+ " is even number");
+		}
+		else{
+			System.out.println(i);
+		}
+		}
+	}
+
+}
